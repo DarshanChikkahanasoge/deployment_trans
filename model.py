6bbf967f62f267a7231ae0bc4063a944f12dffb0
Darshan C B <54507300+DarshanChikkahanasoge@users.noreply.github.com>
2022-10-09 19:43:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -14,17 +14,14 @@ del to_update['TransactionDate']
 x=to_update.drop(['IsFraud'],axis=1)
 y=to_update['IsFraud']
 
-#print(x.head())
 
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 x['TransactionID']=le.fit_transform(x.TransactionID.values)
-#print(x.head())
 
 
 sc=StandardScaler()
 x3=sc.fit_transform(x)
-#print(x3[:5,:])
 
 xtrain,xtest,ytrain,ytest=train_test_split(np.array(x3),np.array(y),test_size=0.3,shuffle=True)
 
@@ -34,31 +31,10 @@ xtrain,xtest,ytrain,ytest=train_test_split(np.array(x3),np.array(y),test_size=0.
 from sklearn.ensemble import RandomForestClassifier
 rf_clf = RandomForestClassifier(n_estimators=100,max_depth=10,random_state=42,
                                 verbose=1,class_weight="balanced")
-#print(xtrain[0])
+
 rf_clf.fit(xtrain,ytrain)
 
 pickle.dump(rf_clf,open('model.pkl','wb'))
-#y_pred1=rf_clf.predict(xtest)
-
-
-
-#print("Classification Report for Random Forest: \n", classification_report(ytest, y_pred1))
-
-#print("Confusion Matrix of Random Forest: \n", confusion_matrix(ytest,y_pred1))
-
-
-from sklearn.metrics import accuracy_score
-#print('the accuracy is :',accuracy_score(ytest,y_pred1))
-
-#id=le.transform(np.array(['T5']))
-#print(id[0])
-
-#l=np.array([[id[0],8300,0,'9088909903']])
-#l=sc.transform(l)
-#print(l)
-#print(rf_clf.predict(l)==1)
-#model = pickle.load(open('model.pkl','rb'))
-#print(model.predict(l))
 
 
 app=Flask(__name__)
